e83da3ef2e293d016c5eebe455c10bcc2ad5f452
Corentin LE FUR <corentin@yellowcake.net>
2020-07-15 14:10:57 +0200

    Ajout d'une page 404

---
+++ src/components/error404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import heroStyles from "../styles/hero.module.css"
+
+const HeroComponent = () => (
+    <div className={heroStyles.hero}>
+        <h1>404 Page Not Found</h1>
+        <p>The page you're looking for could not be found.</p>
+    </div>
+)
+
+export default HeroComponent
---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Error404 from "../components/error404"
+
+const IndexPage = () => (
+    <div style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    }}>
+        <Error404 />
+    </div>
+)
+
+export default IndexPage;
