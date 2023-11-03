4aacdaff02fea7b75f3a68190182e981bf7ea67d
Corentin LE FUR <corentin.lf@gmail.com>
2023-11-03 11:45:03 +0100

    Init project

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header className='header'>
+      <h1>
+        <small>Corentin Le Fur</small>
+        <span>Développeur web <em>freelance</em></span>
+      </h1>
+
+      <div className='disponibility'>
+        <span className='disponibility__indicator'></span>
+        Disponible pour de nouvelles opportunités
+      </div>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque laboriosam reiciendis fuga, ratione est et blanditiis libero beatae at. Eveniet voluptatem mollitia consequatur quasi iusto ipsum assumenda libero deleniti repudiandae!</p>
+
+      <div className="scroll-down">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+          <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
+        </svg>
+      </div>
+    </header>
+  )
+}
+
+export default Header
